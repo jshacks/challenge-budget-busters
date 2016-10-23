211138d4d5db3aa3e6a37848e19b4add066fe966
Mihail Serafim <contact@mihailserafim.ro>
2016-10-23 14:59:57 +0300

    Added comment fetching

--- app/Modules/Landing/Controller/Landing.js
+++ app/Modules/Landing/Controller/Landing.js
@@ -27,45 +27,130 @@
         var self = this;
 
         $scope.getComments = function () {
-            $http.get('/api', { params: user },
-              function (response) { $scope.results = response; },
-              function (failure) { console.log("failed :(", failure); });
+            $http.get('/api/proposals')
+              .then(
+              function (response) { $scope.comments = response; },
+              function (failure) { console.log("failed :(", failure) } );
         }
 
-        $scope.comments = [
-              {
-                id: 1,
-                name: 'Eusebiu Schipor',
-                photo: 'http://lorempixel.com/40/40/people/1/',
-                comment: 'Mai multi bani pentru biserici!!!!',
-                date: '2016-10-23',
-                amount: '9455 lei'
-              },
-              {
-                id: 2,
-                name: 'Mihail Serafim',
-                photo: 'http://lorempixel.com/40/40/people/2/',
-                comment: 'Pentru spitale cred ca e prea mare bugetul.',
-                date: '2016-10-23',
-                amount: '158700 lei'
-              },
-              {
-                id: 3,
-                name: 'Paul Raetchi',
-                photo: 'http://lorempixel.com/40/40/people/3/',
-                comment: 'Trebuie sa luam tot bugetul alocat pentru biserici si sa-l folosim pentru modernizarea cluburilor.',
-                date: '2016-10-22',
-                amount: '1500 lei'
-              },
-              {
-                id: 4,
-                name: 'Bogdan Luca',
-                photo: 'http://lorempixel.com/40/40/people/4/',
-                comment: 'De ce suntem asa saraci??? Vrem mai multi bani pentru scoli!',
-                date: '2016-10-22',
-                amount: '2000 lei'
-              },
-            ];
+        var cheltuieli = [{
+                            id: 'curente',
+                            name: 'Cheltuieli Curente',
+                            color: '#00FF00'
+                        },
+                        {
+                            name: 'Cheltuieli de personal',
+                            parent: 'curente',
+                            value: 117366
+                        },
+                        {
+                            name: 'Bunuri si servicii',
+                            parent: 'curente',
+                            value: 590569
+                        },
+                        {
+                            name: 'Dobanzi',
+                            parent: 'curente',
+                            value: 124540
+                        },
+                        {
+                            name: 'Subventii',
+                            parent: 'curente',
+                            value: 1059000
+                        },
+                        {
+                            name: 'Fonduri de rezerva',
+                            parent: 'curente',
+                            value: 12175
+                        },
+                        {
+                            name: 'Transferuri intre unitati ale administratiei',
+                            parent: 'curente',
+                            value: 627437
+                        },
+                        {
+                            name: 'Alte transferuri',
+                            parent: 'curente',
+                            value: 73878
+                        },
+                        {
+                            name: 'Proiecte cu finantare din Fonduri externe nerambursabile postaderare',
+                            parent: 'curente',
+                            value: 463174
+                        },
+                        {
+                            name: 'Asistenta sociala',
+                            parent: 'curente',
+                            value: 1682
+                        },
+                        {
+                            name: 'Alte cheltuieli',
+                            parent: 'curente',
+                            value: 112860
+                        },
+                        {
+                            name: 'Cheltuieli de capital',
+                            value: 823638,
+                            color: '#0000FF'
+                        },
+                        {
+                            name: 'Operatiuni financiare',
+                            id: 'operatiuni',
+                            color: '#FF0000'
+                        },
+                        {
+                            name: 'Imprumuturi acordate',
+                            parent: 'operatiuni',
+                            value: 0
+                        },
+                        {
+                            name: 'Rambursari de credite externe si interne',
+                            parent: 'operatiuni',
+                            value: 133677
+                        },
+                        {
+                            name: 'Plati efectuate in anii precedenti si recuperate in anul curent',
+                            color: '#FFFF00',
+                            value: 0
+                        },
+                        {
+                            name: 'Rezerve',
+                            color: '#00FFFF',
+                            value: 0
+                        }];
+
+            $('#container').highcharts({
+                series: [{
+                    type: 'treemap',
+                    layoutAlgorithm: 'squarified',
+                    allowDrillToNode: true,
+                    animationLimit: 1000,
+                    dataLabels: {
+                        enabled: false
+                    },
+                    levelIsConstant: false,
+                    levels: [{
+                        level: 1,
+                        dataLabels: {
+                            enabled: true
+                        },
+                        borderWidth: 3
+                    }],
+                    data: cheltuieli,
+                    events: {
+                        click: function (event) {
+                            $('#categoryTitle').text(event.point.name);
+                            $('#categoryValue').text(event.point.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' RON');
+                            $scope.getComments();
+                        }
+                    }
+                }],
+                title: {
+                    text: 'Bugetul Municipiului Bucuresti'
+                }
+            });
+
+        $scope.comments = [];
 
         this.addNewCommentForm = {};
 
