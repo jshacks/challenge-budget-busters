2f29ab6c5dcec39d7f1855af4a18b7e93f15cebb
Diana Miron <miron.diana.gabriela@gmail.com>
2016-10-23 13:31:58 +0300

    made nav blue except home

--- app/Modules/Landing/Controller/LandingController.js
+++ app/Modules/Landing/Controller/LandingController.js
@@ -4,6 +4,6 @@
     var app = angular.module('Landing', []);
 
     app.controller('LandingController', ['$scope', '$http', '$window', '$location', '$routeParams', function($scope, $http, $window, $location, $routeParams) {
-        $scope.landing = {};
+        $scope.landing = {};  
     }]);
 })();
